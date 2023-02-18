c8af38df7eca3d4c104e3ebbdbef0aa6bf797127
FoxxFranc <74159513+FoxxFranc@users.noreply.github.com>
2023-02-18 21:53:53 +0300

    Transformer

---
+++ Assets/Scripts/Runtime/Devices/Interactable.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Model
+{
+    public class Interactable : MonoBehaviour
+    {
+        [SerializeField] private Sprite _closeState;
+        [SerializeField] private Sprite _openState;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void Open()
+        {
+            _spriteRenderer.sprite = _openState;
+        }
+
+        public void Close()
+        {
+            _spriteRenderer.sprite = _closeState;
+        }
+    }
+}
