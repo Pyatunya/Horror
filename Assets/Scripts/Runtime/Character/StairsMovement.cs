aa18afb10099aea87769acce44f169dc13c2e37e
FoxxFranc <74159513+FoxxFranc@users.noreply.github.com>
2023-02-19 00:00:42 +0300

    Upstairs

--- Assets/Scripts/Runtime/Character/StairsMovement.cs
+++ Assets/Scripts/Runtime/Character/StairsMovement.cs
@@ -6,6 +6,50 @@ namespace Game.Model
 {
     public class StairsMovement : MonoBehaviour
     {
-        
+        [SerializeField] private StairPoint _firstFloor;
+        [SerializeField] private StairPoint _secondFloor;
+
+        private bool isOnFirstFloor;
+        private StairPoint _currentStairPoint;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent<StairPoint>(out StairPoint stairPoint))
+            {                
+                if(stairPoint == _firstFloor)
+                {
+                    _currentStairPoint = stairPoint;
+                    isOnFirstFloor = true;
+                }
+                else
+                {
+                    _currentStairPoint = _secondFloor;
+                    isOnFirstFloor = false;
+                }
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent<StairPoint>(out StairPoint stairPoint))
+            {
+                _currentStairPoint = null;
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E) && _currentStairPoint != null)
+            {
+                if (isOnFirstFloor)
+                {
+                    transform.position = _secondFloor.transform.position;
+                }
+                else
+                {
+                    transform.position = _firstFloor.transform.position;
+                }
+            }
+        }
     }
 }
